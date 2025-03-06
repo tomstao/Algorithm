03e46fa77f0450d3c6d47c82e4d46ba6603fe7b6
tomstao <96sutao@gmail.com>
2025-03-05 20:12:07 -0500

    Radix sort

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 //        int[] intArray = Algorithms.generateRandomArray(10,10);
-        int[] intArray2 = Algorithms.generateRandomArray(10,5000, 1000);
+        int[] intArray2 = Algorithms.generateRandomArray(10,5000, 100);
         //Algorithms.selection(intArray);
         //Algorithms.insertionSort(intArray);
         //Algorithms.shellSort(intArray);
