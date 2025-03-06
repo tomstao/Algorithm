44b86ceab13a883eeae1f88eac45271abd228f7a
tomstao <96sutao@gmail.com>
2025-03-05 19:10:15 -0500

    Radix sort

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -159,11 +159,51 @@ public class Algorithms {
         }
     }
 
-    public static int[] generateRandomArray(int size, int bound) {
+    static void radixSort(int[] input, int radix, int width) {
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSort(int[] input, int position, int radix) {
+        int numItems = input.length;
+
+        int[] countArray = new int[radix];
+
+        for(int value : input){
+            countArray[gerDigit(position, value, radix)]++;
+        }
+
+        //adjust the count array;
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        int[] temp = new int[numItems];
+        for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--){
+            temp[--countArray[gerDigit(position, input[tempIndex], radix)]] = input[tempIndex];
+        }
+
+        for(int tempIndex = 0; tempIndex < numItems; tempIndex++){
+            input[tempIndex] = temp[tempIndex];
+        }
+
+    }
+
+    public static int gerDigit(int position, int value, int radix) {
+        return value /  (int) Math.pow(10, position) % radix;
+
+    }
+
+    public static int[] generateRandomArray(int size, int bound, int lowerBound) {
         int[] array = new int[size];
         Random random = new Random();
         for (int i = 0; i < size; i++) {
-            array[i] = random.nextInt(bound - 1) + 1;
+            int rand = random.nextInt(bound);
+            while (rand < lowerBound) {
+                rand = random.nextInt(bound);
+            }
+            array[i] = rand;
         }
 
         return array;
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int[] intArray = Algorithms.generateRandomArray(10,10);
+//        int[] intArray = Algorithms.generateRandomArray(10,10);
+        int[] intArray2 = Algorithms.generateRandomArray(10,5000, 1000);
         //Algorithms.selection(intArray);
         //Algorithms.insertionSort(intArray);
         //Algorithms.shellSort(intArray);
         //Algorithms.mergeSort(intArray, 0, intArray.length);
         //Algorithms.quickSort(intArray, 0, intArray.length);
-        Algorithms.printArray(intArray);
+//        Algorithms.printArray(intArray);
+//        System.out.println();
+//        Algorithms.countingSort(intArray,1,10);
+//        Algorithms.printArray(intArray);
+        Algorithms.printArray(intArray2);
+        Algorithms.radixSort(intArray2,10,4);
         System.out.println();
-        Algorithms.countingSort(intArray,1,10);
-        Algorithms.printArray(intArray);
+        Algorithms.printArray(intArray2);
+
     }
 }
